44bd5fa2ec9a7391b6ff02c01a5198d9d5e6199d
Greissy Naranjo <greissynv@gmail.com>
2024-12-12 09:27:15 +0100

    cambios

--- js.js
+++ js.js
@@ -21,16 +21,18 @@
         var brickOffsetTop = 30;
         var brickOffsetLeft = 30;
 
+        document.addEventListener("keydown", keyDownHandler, false);
+        document.addEventListener("keyup", keyUpHandler, false);
+
         var bricks = [];
         for (c = 0; c < brickColumnCount; c++) {
             bricks[c] = [];
             for (r = 0; r < brickRowCount; r++) {
-              bricks[c][r] = { x: 0, y: 0 };
+              bricks[c][r] = { x: 0, y: 0, status: 1 };
             }
         }
 
-        document.addEventListener("keydown", keyDownHandler, false);
-        document.addEventListener("keyup", keyUpHandler, false);
+        
 
         function keyDownHandler(e) {
             if(e.keyCode == 39) {
@@ -47,6 +49,49 @@
                 leftPressed = false;
              }
             }
+
+
+
+            function drawBricks() {
+                for (c = 0; c < brickColumnCount; c++) {
+                  for (r = 0; r < brickRowCount; r++) {
+                    if (bricks[c][r].status == 1) {
+                      var brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+                      var brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+                      bricks[c][r].x = brickX;
+                      bricks[c][r].y = brickY;
+                      ctx.beginPath();
+                      ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                      ctx.fillStyle = "#0095DD";
+                      ctx.fill();
+                      ctx.closePath();
+                    }
+                  }
+                }
+              }
+
+              
+
+              function collisionDetection() {
+                for (c = 0; c < brickColumnCount; c++) {
+                  for (r = 0; r < brickRowCount; r++) {
+                    var b = bricks[c][r];
+                    if (b.status == 1) {
+                      if (
+                        x > b.x &&
+                        x < b.x + brickWidth &&
+                        y > b.y &&
+                        y < b.y + brickHeight
+                      ) {
+                        dy = -dy;
+                        b.status = 0;
+                      }
+                    }
+                  }
+                }
+              }
+              
+
 
         function drawBall() {
                 ctx.beginPath();
